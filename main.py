36d291b67895612ea2318c3c15b2ac48fdd0ef3b
Motwg <piruci88@gmail.com>
2022-11-12 00:45:51 +0100

    Just some deleted not important code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 
 from app.data_utils import create_df
@@ -10,6 +9,3 @@ if __name__ == '__main__':
     # "short" for testing and debugging, "full" for production
     df = create_df('files/NYPD_short.csv')
     print(df.head(10))
-
-    df = pd.read_csv(os.path.join('app/profiler/data', 'data_full.csv'), low_memory=False, index_col=False)
-    print(df.isnull().sum())
