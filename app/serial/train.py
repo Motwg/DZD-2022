34d1528b34b7b0c8e59664758a8c221ffca05abb
224094 Lemiesz Aleksander <224094@edu.p.lodz.pl>
2022-11-14 23:27:10 +0100

    Improved display

--- app/serial/train.py
+++ app/serial/train.py
@@ -7,14 +7,16 @@ if __name__ == '__main__':
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
     df = create_df('../../files/NYPD_short.csv')
-    print(df.head(10))
     df, orig_df = serial_pipeline(df)
-    # df.to_csv('NYPD_short.csv')
-    print(df.head(10))
 
     neighbors = NearestNeighbors(algorithm='ball_tree').fit(df)
     dist, indices = neighbors.kneighbors(df)
-    print(indices)
-    for j in range(4):
-        for i in range(142675):
-            print('Ind: ' + str(indices[i][j]) + ', dist: ' + str(dist[i][j]))
+    # for j in range(142675):
+    for j in range(1):
+        print('Crimes similar to crime number ' + str(j))
+        for i in range(4):
+            if dist[j][i] < 10.0:
+                print('Crime' + str(i) + ': ' + str(orig_df.iloc[[indices[j][i]]]))
+                print('Distance: ' + str(dist[j][i]) + '\n')
+            if i == 4:
+                print('\n')
